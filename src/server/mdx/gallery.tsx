98b33e51ad74a3cccabab8a550666b6fadbaa6b8
floatflower <floatflower1029@gmail.com>
2026-03-19 23:31:25 +0800

    Update Gallery

--- src/server/mdx/gallery.tsx
+++ src/server/mdx/gallery.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useState } from "react";
+import type React from "react";
 import Lightbox from "yet-another-react-lightbox";
 import Captions from "yet-another-react-lightbox/plugins/captions";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 interface GalleryImage {
   url: string;
@@ -45,7 +48,15 @@ export function Gallery({ images }: { images: GalleryImage[] }) {
         close={() => setOpen(false)}
         index={index}
         slides={slides}
-        plugins={[Captions]}
+        plugins={[Captions, Thumbnails]}
+        thumbnails={{ position: "bottom", width: 64, height: 64, gap: 8 }}
+        styles={{
+          // @ts-ignore
+          root: {
+            "--yarl__color_backdrop": "rgba(0, 0, 0, 0.8)",
+            backdropFilter: "blur(12px)",
+          } as React.CSSProperties,
+        }}
       />
     </>
   );
