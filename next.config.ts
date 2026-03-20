7c424d258868bfe25da4fac7c373c4dcf73dd7ba
floatflower <floatflower1029@gmail.com>
2026-03-20 16:56:53 +0800

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export',
+  output: 'export',
   allowedDevOrigins: ['192.168.1.101'],
   reactStrictMode: false,
   trailingSlash: true,
